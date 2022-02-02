2672474e54be2a2ee380386c2b4478e5057515be
BaraahAbusara <baraaabusara@gmail.com>
2022-02-02 09:42:39 +0200

    Finally Done

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS Movie;
 CREATE TABLE IF NOT EXISTS Movie (
     id  SERIAL PRIMARY KEY ,
     title VARCHAR (225),
-    release_date DATE (225),
-    poster_path IMAGE (225),
-    overview TEXT (4000)
+    release_date VARCHAR (225),
+    poster_path VARCHAR (225),
+    overview VARCHAR (4000)
 );
